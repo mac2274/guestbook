2da9b76d7b4e062f649d04ed32e74a2f5e69ab51
Maggi <yuen.maggi@gmail.com>
2025-05-20 10:49:20 +0200

    commit

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$mysqli = new mysqli('localhost', 'guestbook', '*YAVm*5f5@g6K3Zz', 'guestbook');
+require_once 'config.db.php';
 
-if($mysqli -> connect_errno){
-    throw new RuntimeException('mysqli-Verbindungsfehler: ' . $mysqli->connect_error);
-}
-
+echo 'done!';
 ?>
