2f29cbc0997b7fe580e4f5a6d826cbf9bb2237f4
Maggi <yuen.maggi@gmail.com>
2025-05-22 09:47:45 +0200

    btbefore

--- index.php
+++ index.php
@@ -3,47 +3,16 @@
 require_once 'config.db.php';
 require_once 'lib.php';
 
-//echo 'Fertig!!';
+echo 'Fertig!!';
 
-// $x = false;
+$x = false;
 
-// if($x=false){
-//     require 'query.php';
-// } else if ($x=true){
-//     require 'prepared.php';
-// }
-
-
-
-//query-statement nur wenn keine persönlichen daten abgefragt werden
-
-// if(isset($_POST['frm_save'])){
-//     if(gb_function($_POST['frm_name'],$_POST['frm_msg'])){
-//         echo 'Der Eintrag wurde erfolgreich hinzugefügt.';
-//     }
-// }
-
-// $query = "SELECT name, nachricht FROM daybook ORDER BY daybook_id DESC LIMIT 3";
-// $result = $mysqli->query($query);
-
-if(isset($_GET['id'])){
-    $id=$_GET['id'];
-
-    $sql = "SELECT * FROM daybook WHERE daybook_id=?";
-
-    if($stmt = $mysqli->prepare($sql)){
-        $stmt->bind_param("i",$id);
-        $stmt->execute();
-
-        if(!$stmt->errno){
-            //handle error here
-        }
-        $result = $stmt->get_result();
-        while($row = $result->fetch_assoc()){
-            echo $row['name'];
-        }
-    }
+if($x==false){
+    require 'query.php';
+} else if ($x==true){
+    require 'prepared.php';
 }
+
 
 while($row = $result->fetch_assoc()){
     echo '<hr><a href="?id='.$row['daybook_id'].'">#'.$row['daybook_id'].'</a><b>'. htmlspecialchars($row['name']) . '</b>: '. htmlspecialchars($row['nachricht']) . '<br>';
