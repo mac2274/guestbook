924fbc557dcc4faf27109151843de24ce3a24fa7
Maggi <yuen.maggi@gmail.com>
2025-05-22 07:58:17 +0200

    new index with preparedStatemenbt

--- index_pStmt.php
+++ index_pStmt.php
@@ -14,24 +14,6 @@ if(isset($_POST['frm_save'])){
 
 ?>
 
-<!-- <script>
-//kurzer Abstecher in die logit von (!ausage)
-if (!hallo()) {
-  console.log("Fertig");
-}
-
-function hallo() {
-let x = false;
-  if (x == true) {
-    console.log("ICH BIN WAHR!");
-    return true;
-  } else if (x == false) {
-    console.log("ICH BIN FALSCH!");
-    return false;
-  }
-}
-</script> -->
-
 <h1>Gästebuch</h1>
 <h2>Letzte Einträge</h2>
 
